5953784fc88c768b823e057fa6d28cba26d12a8f
Indibar <indibar@algo2trade.com>
2019-11-30 16:34:46 +0530

    Add project files.

---
+++ EOD Positional Data Backup/frmMain.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.lblProgress = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.lblOverallProgress = New System.Windows.Forms.Label()
+        Me.btnStop = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(706, 390)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(85, 29)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'lblProgress
+        '
+        Me.lblProgress.Location = New System.Drawing.Point(3, 395)
+        Me.lblProgress.Name = "lblProgress"
+        Me.lblProgress.Size = New System.Drawing.Size(694, 50)
+        Me.lblProgress.TabIndex = 2
+        Me.lblProgress.Text = "Progress Status ....."
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(2, 3)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(796, 354)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'lblOverallProgress
+        '
+        Me.lblOverallProgress.Location = New System.Drawing.Point(3, 363)
+        Me.lblOverallProgress.Name = "lblOverallProgress"
+        Me.lblOverallProgress.Size = New System.Drawing.Size(795, 26)
+        Me.lblOverallProgress.TabIndex = 4
+        Me.lblOverallProgress.Text = "Overall Progress Status ....."
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(706, 421)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(85, 25)
+        Me.btnStop.TabIndex = 5
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.lblOverallProgress)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.lblProgress)
+        Me.Controls.Add(Me.btnStart)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "EOD Positional Data Backup"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents lblProgress As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents lblOverallProgress As Label
+    Friend WithEvents btnStop As Button
+End Class
---
+++ EOD Positional Data Backup/frmMain.vb
@@ -0,0 +1,308 @@
+﻿Imports Algo2TradeBLL
+Imports Utilities.DAL
+Imports System.Threading
+
+Public Class frmMain
+
+#Region "Common Delegates"
+    Delegate Sub SetObjectEnableDisable_Delegate(ByVal [obj] As Object, ByVal [value] As Boolean)
+    Public Sub SetObjectEnableDisable_ThreadSafe(ByVal [obj] As Object, ByVal [value] As Boolean)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [obj].InvokeRequired Then
+            Dim MyDelegate As New SetObjectEnableDisable_Delegate(AddressOf SetObjectEnableDisable_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[obj], [value]})
+        Else
+            [obj].Enabled = [value]
+        End If
+    End Sub
+
+    Delegate Sub SetObjectVisible_Delegate(ByVal [obj] As Object, ByVal [value] As Boolean)
+    Public Sub SetObjectVisible_ThreadSafe(ByVal [obj] As Object, ByVal [value] As Boolean)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [obj].InvokeRequired Then
+            Dim MyDelegate As New SetObjectVisible_Delegate(AddressOf SetObjectVisible_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[obj], [value]})
+        Else
+            [obj].Visible = [value]
+        End If
+    End Sub
+
+    Delegate Sub SetLabelText_Delegate(ByVal [label] As Label, ByVal [text] As String)
+    Public Sub SetLabelText_ThreadSafe(ByVal [label] As Label, ByVal [text] As String)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [label].InvokeRequired Then
+            Dim MyDelegate As New SetLabelText_Delegate(AddressOf SetLabelText_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[label], [text]})
+        Else
+            [label].Text = [text]
+        End If
+    End Sub
+
+    Delegate Function GetLabelText_Delegate(ByVal [label] As Label) As String
+    Public Function GetLabelText_ThreadSafe(ByVal [label] As Label) As String
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [label].InvokeRequired Then
+            Dim MyDelegate As New GetLabelText_Delegate(AddressOf GetLabelText_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[label]})
+        Else
+            Return [label].Text
+        End If
+    End Function
+
+    Delegate Sub SetLabelTag_Delegate(ByVal [label] As Label, ByVal [tag] As String)
+    Public Sub SetLabelTag_ThreadSafe(ByVal [label] As Label, ByVal [tag] As String)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [label].InvokeRequired Then
+            Dim MyDelegate As New SetLabelTag_Delegate(AddressOf SetLabelTag_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[label], [tag]})
+        Else
+            [label].Tag = [tag]
+        End If
+    End Sub
+
+    Delegate Function GetLabelTag_Delegate(ByVal [label] As Label) As String
+    Public Function GetLabelTag_ThreadSafe(ByVal [label] As Label) As String
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [label].InvokeRequired Then
+            Dim MyDelegate As New GetLabelTag_Delegate(AddressOf GetLabelTag_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[label]})
+        Else
+            Return [label].Tag
+        End If
+    End Function
+    Delegate Sub SetToolStripLabel_Delegate(ByVal [toolStrip] As StatusStrip, ByVal [label] As ToolStripStatusLabel, ByVal [text] As String)
+    Public Sub SetToolStripLabel_ThreadSafe(ByVal [toolStrip] As StatusStrip, ByVal [label] As ToolStripStatusLabel, ByVal [text] As String)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [toolStrip].InvokeRequired Then
+            Dim MyDelegate As New SetToolStripLabel_Delegate(AddressOf SetToolStripLabel_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[toolStrip], [label], [text]})
+        Else
+            [label].Text = [text]
+        End If
+    End Sub
+
+    Delegate Function GetToolStripLabel_Delegate(ByVal [toolStrip] As StatusStrip, ByVal [label] As ToolStripLabel) As String
+    Public Function GetToolStripLabel_ThreadSafe(ByVal [toolStrip] As StatusStrip, ByVal [label] As ToolStripLabel) As String
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [toolStrip].InvokeRequired Then
+            Dim MyDelegate As New GetToolStripLabel_Delegate(AddressOf GetToolStripLabel_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[toolStrip], [label]})
+        Else
+            Return [label].Text
+        End If
+    End Function
+
+    Delegate Function GetDateTimePickerValue_Delegate(ByVal [dateTimePicker] As DateTimePicker) As Date
+    Public Function GetDateTimePickerValue_ThreadSafe(ByVal [dateTimePicker] As DateTimePicker) As Date
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [dateTimePicker].InvokeRequired Then
+            Dim MyDelegate As New GetDateTimePickerValue_Delegate(AddressOf GetDateTimePickerValue_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New DateTimePicker() {[dateTimePicker]})
+        Else
+            Return [dateTimePicker].Value
+        End If
+    End Function
+
+    Delegate Function GetNumericUpDownValue_Delegate(ByVal [numericUpDown] As NumericUpDown) As Integer
+    Public Function GetNumericUpDownValue_ThreadSafe(ByVal [numericUpDown] As NumericUpDown) As Integer
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [numericUpDown].InvokeRequired Then
+            Dim MyDelegate As New GetNumericUpDownValue_Delegate(AddressOf GetNumericUpDownValue_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New NumericUpDown() {[numericUpDown]})
+        Else
+            Return [numericUpDown].Value
+        End If
+    End Function
+
+    Delegate Function GetComboBoxIndex_Delegate(ByVal [combobox] As ComboBox) As Integer
+    Public Function GetComboBoxIndex_ThreadSafe(ByVal [combobox] As ComboBox) As Integer
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [combobox].InvokeRequired Then
+            Dim MyDelegate As New GetComboBoxIndex_Delegate(AddressOf GetComboBoxIndex_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[combobox]})
+        Else
+            Return [combobox].SelectedIndex
+        End If
+    End Function
+
+    Delegate Function GetComboBoxItem_Delegate(ByVal [ComboBox] As ComboBox) As String
+    Public Function GetComboBoxItem_ThreadSafe(ByVal [ComboBox] As ComboBox) As String
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [ComboBox].InvokeRequired Then
+            Dim MyDelegate As New GetComboBoxItem_Delegate(AddressOf GetComboBoxItem_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[ComboBox]})
+        Else
+            Return [ComboBox].SelectedItem.ToString
+        End If
+    End Function
+
+    Delegate Function GetTextBoxText_Delegate(ByVal [textBox] As TextBox) As String
+    Public Function GetTextBoxText_ThreadSafe(ByVal [textBox] As TextBox) As String
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [textBox].InvokeRequired Then
+            Dim MyDelegate As New GetTextBoxText_Delegate(AddressOf GetTextBoxText_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[textBox]})
+        Else
+            Return [textBox].Text
+        End If
+    End Function
+
+    Delegate Function GetCheckBoxChecked_Delegate(ByVal [checkBox] As CheckBox) As Boolean
+    Public Function GetCheckBoxChecked_ThreadSafe(ByVal [checkBox] As CheckBox) As Boolean
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [checkBox].InvokeRequired Then
+            Dim MyDelegate As New GetCheckBoxChecked_Delegate(AddressOf GetCheckBoxChecked_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[checkBox]})
+        Else
+            Return [checkBox].Checked
+        End If
+    End Function
+
+    Delegate Function GetRadioButtonChecked_Delegate(ByVal [radioButton] As RadioButton) As Boolean
+    Public Function GetRadioButtonChecked_ThreadSafe(ByVal [radioButton] As RadioButton) As Boolean
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [radioButton].InvokeRequired Then
+            Dim MyDelegate As New GetRadioButtonChecked_Delegate(AddressOf GetRadioButtonChecked_ThreadSafe)
+            Return Me.Invoke(MyDelegate, New Object() {[radioButton]})
+        Else
+            Return [radioButton].Checked
+        End If
+    End Function
+
+    Delegate Sub SetDatagridBindDatatable_Delegate(ByVal [datagrid] As DataGridView, ByVal [table] As DataTable)
+    Public Sub SetDatagridBindDatatable_ThreadSafe(ByVal [datagrid] As DataGridView, ByVal [table] As DataTable)
+        ' InvokeRequired required compares the thread ID of the calling thread to the thread ID of the creating thread.  
+        ' If these threads are different, it returns true.  
+        If [datagrid].InvokeRequired Then
+            Dim MyDelegate As New SetDatagridBindDatatable_Delegate(AddressOf SetDatagridBindDatatable_ThreadSafe)
+            Me.Invoke(MyDelegate, New Object() {[datagrid], [table]})
+        Else
+            [datagrid].DataSource = [table]
+            [datagrid].Refresh()
+        End If
+    End Sub
+#End Region
+
+#Region "Event Handlers"
+    Private Sub OnHeartbeat(message As String)
+        SetLabelText_ThreadSafe(lblProgress, message)
+    End Sub
+    Private Sub OnHeartbeatMain(message As String)
+        SetLabelText_ThreadSafe(lblOverallProgress, message)
+    End Sub
+    Private Sub OnDocumentDownloadComplete()
+        'OnHeartbeat("Document download compelete")
+    End Sub
+    Private Sub OnDocumentRetryStatus(currentTry As Integer, totalTries As Integer)
+        OnHeartbeat(String.Format("Try #{0}/{1}: Connecting...", currentTry, totalTries))
+    End Sub
+    Public Sub OnWaitingFor(ByVal elapsedSecs As Integer, ByVal totalSecs As Integer, ByVal msg As String)
+        OnHeartbeat(String.Format("{0}, waiting {1}/{2} secs", msg, elapsedSecs, totalSecs))
+    End Sub
+#End Region
+
+    Private canceller As CancellationTokenSource
+
+    Private Async Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        canceller = New CancellationTokenSource
+        Await Task.Run(AddressOf StartProcessingAsync).ConfigureAwait(False)
+    End Sub
+
+    Private Async Function StartProcessingAsync() As Task
+        Try
+            Dim stockList As List(Of String) = Await GetStockListAsync().ConfigureAwait(False)
+            Dim cmn As Common = New Common(canceller)
+            AddHandler cmn.Heartbeat, AddressOf OnHeartbeat
+            AddHandler cmn.DocumentDownloadComplete, AddressOf OnDocumentDownloadComplete
+            AddHandler cmn.DocumentRetryStatus, AddressOf OnDocumentRetryStatus
+            AddHandler cmn.WaitingFor, AddressOf OnWaitingFor
+
+            If stockList IsNot Nothing AndAlso stockList.Count > 0 Then
+                Using sqlHlpr As New MySQLDBHelper(My.Settings.ServerName, "local_stock", "3306", "rio", "speech123", canceller)
+                    AddHandler sqlHlpr.Heartbeat, AddressOf OnHeartbeat
+                    AddHandler sqlHlpr.DocumentDownloadComplete, AddressOf OnDocumentDownloadComplete
+                    AddHandler sqlHlpr.DocumentRetryStatus, AddressOf OnDocumentRetryStatus
+                    AddHandler sqlHlpr.WaitingFor, AddressOf OnWaitingFor
+                    Dim counter As Integer = 0
+                    For Each runningStock In stockList
+                        canceller.Token.ThrowIfCancellationRequested()
+                        counter += 1
+                        OnHeartbeatMain(String.Format("Processing for {0} ({1}/{2})", runningStock, counter, stockList.Count))
+                        Dim eodHistoricalData As Dictionary(Of Date, Payload) = Await cmn.GetHistoricalDataAsync(Common.DataBaseTable.EOD_Cash, runningStock, Now.Date.AddYears(-5), Now.Date)
+                        If eodHistoricalData IsNot Nothing AndAlso eodHistoricalData.Count > 0 Then
+                            Dim insertDataString As String = Nothing
+                            For Each runningPayload In eodHistoricalData.Values
+                                canceller.Token.ThrowIfCancellationRequested()
+                                insertDataString = String.Format("{0},('{1}',{2},{3},{4},{5},{6},'{7}',TIMESTAMP(CURRENT_TIME))",
+                                                                 insertDataString,
+                                                                 runningPayload.TradingSymbol,
+                                                                 runningPayload.Open,
+                                                                 runningPayload.Low,
+                                                                 runningPayload.High,
+                                                                 runningPayload.Close,
+                                                                 runningPayload.Volume,
+                                                                 runningPayload.PayloadDate.ToString("yyyy-MM-dd"))
+                            Next
+                            If insertDataString IsNot Nothing Then
+                                Dim insertString As String = String.Format("INSERT INTO `eod_positional_data` (`TradingSymbol`,`Open`,`Low`,`High`,`Close`,`Volume`,`SnapshotDate`,`UpdateToDBTime`) VALUES {0} ON DUPLICATE KEY UPDATE `TradingSymbol`=VALUES(`TradingSymbol`), `Open`=VALUES(`Open`), `Low`=VALUES(`Low`), `High`=VALUES(`High`), `Close`=VALUES(`Close`), `Volume`=VALUES(`Volume`), `SnapshotDate`=VALUES(`SnapshotDate`), `UpdateToDBTime`=VALUES(`UpdateToDBTime`);", insertDataString.Substring(1))
+
+                                canceller.Token.ThrowIfCancellationRequested()
+                                Dim numberOfdata As Integer = Await sqlHlpr.RunUpdateAsync(insertString).ConfigureAwait(False)
+
+                            End If
+                        End If
+                    Next
+                End Using
+            End If
+        Catch cex As OperationCanceledException
+            MsgBox(cex.Message)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        Finally
+            SetLabelText_ThreadSafe(lblProgress, "")
+            SetLabelText_ThreadSafe(lblOverallProgress, "Process Complete")
+        End Try
+    End Function
+
+    Private Async Function GetStockListAsync() As Task(Of List(Of String))
+        Dim ret As List(Of String) = Nothing
+        Using sqlHlpr As New MySQLDBHelper(My.Settings.ServerName, "local_stock", "3306", "rio", "speech123", canceller)
+            AddHandler sqlHlpr.Heartbeat, AddressOf OnHeartbeat
+            AddHandler sqlHlpr.DocumentDownloadComplete, AddressOf OnDocumentDownloadComplete
+            AddHandler sqlHlpr.DocumentRetryStatus, AddressOf OnDocumentRetryStatus
+            AddHandler sqlHlpr.WaitingFor, AddressOf OnWaitingFor
+
+            Dim dt As DataTable = Await sqlHlpr.RunSelectAsync("SELECT DISTINCT(`TRADING_SYMBOL`) FROM `active_instruments_cash`").ConfigureAwait(False)
+            canceller.Token.ThrowIfCancellationRequested()
+            If dt IsNot Nothing AndAlso dt.Rows.Count > 0 Then
+                For i = 0 To dt.Rows.Count - 1
+                    canceller.Token.ThrowIfCancellationRequested()
+                    If Not IsDBNull(dt.Rows(i).Item(0)) Then
+                        If ret Is Nothing Then ret = New List(Of String)
+                        ret.Add(dt.Rows(i).Item(0))
+                    End If
+                Next
+            End If
+        End Using
+        Return ret
+    End Function
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        canceller.Cancel()
+    End Sub
+End Class
